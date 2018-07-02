a82e07f8699a6438ab64b85e08d6b1f587c1f26c
Luis Camilo <lhenriquegomescamilo@hotmail.com>
2018-07-01 22:28:00 -0300

    * Criando estrutura basica para o chat ** Enviando mensagem para o firebase com dados mocados

---
+++ src/app/chat.service.ts
@@ -0,0 +1,58 @@
+import {Injectable} from '@angular/core';
+import {ChatMessageModel} from './models/chat-message.model';
+import {FirebaseListObservable} from 'angularfire2/database-deprecated';
+import {Observable, Subject} from 'rxjs';
+import {AngularFireDatabase} from 'angularfire2/database';
+import {AngularFireAuth} from 'angularfire2/auth';
+import * as _ from 'lodash';
+import * as moment from 'moment';
+import {filter, switchMap} from 'rxjs/operators';
+import {AngularFireList, QueryFn} from 'angularfire2/database/interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+  user: any;
+
+  private _chatMessages: AngularFireList<ChatMessageModel | ChatMessageModel[]>;
+  private chatMessage: ChatMessageModel;
+  private username = '';
+
+  // private username: Observable<string>;
+
+  constructor(
+    private db: AngularFireDatabase,
+    private afAuth: AngularFireAuth
+  ) {
+    this.afAuth.authState
+      .pipe(filter(auth => !_.isEmpty(auth)))
+      .subscribe(auth => this.user = auth);
+  }
+
+  sendMessage(message: string): void {
+    if (message) {
+      const timeStamp = this.getTimeStamp();
+      this._chatMessages = this.chatMessages;
+      const chatMessage = {
+        message: message,
+        timeSent: timeStamp,
+        userName: 'the user name is',
+        email: 'teste@teste.com.br'
+      } as ChatMessageModel;
+      this._chatMessages.push(chatMessage);
+      console.log(`Send messaging`, message);
+    }
+
+  }
+
+
+  get chatMessages(): AngularFireList<ChatMessageModel | ChatMessageModel[]> {
+    return this.db.list('messages', ref => ref.limitToFirst(25).orderByKey());
+
+  }
+
+  private getTimeStamp(): string {
+    return moment().format();
+  }
+}
