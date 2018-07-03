7fb7a375dc1a3616cee310b8a8b69072b2e3b84e
Luis Camilo <lhenriquegomescamilo@hotmail.com>
2018-07-03 00:30:49 -0300

    * Criando scroller em chat

--- src/app/chatroom/chatroom.component.ts
+++ src/app/chatroom/chatroom.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-chatroom',
   templateUrl: './chatroom.component.html',
   styleUrls: ['./chatroom.component.scss']
 })
-export class ChatroomComponent implements OnInit {
+export class ChatroomComponent implements OnInit, AfterViewChecked {
 
-  constructor() { }
+
+  @ViewChild('scroller')
+  private feedContainer: ElementRef;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  ngAfterViewChecked(): void {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    this.feedContainer.nativeElement.scrollTop =  this.feedContainer.nativeElement.scrollHeight;
+  }
 }
