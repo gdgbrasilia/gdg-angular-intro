d384b7928b3e3ed2e094c0c6a167aaf4ef816005
Luis Camilo <lhenriquegomescamilo@hotmail.com>
2018-07-03 00:37:00 -0300

    * Criando lista de usuários ativos

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {UserModel} from '../models/user.model';
 
 @Component({
   selector: 'app-user-item',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserItemComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  user: UserModel;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserModel} from '../models/user.model';
+import {ChatService} from '../services/chat.service';
 
 @Component({
   selector: 'app-user-list',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  users: UserModel[];
+
+  constructor(private chatService: ChatService) {
+  }
 
   ngOnInit() {
+    this.chatService.users
+      .subscribe((users: UserModel[]) => this.users = users);
   }
 
 }
